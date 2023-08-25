1ec5c3835c3d5373fd5da70369fd212791b2613b
rafadondee <127406148+rafadondee@users.noreply.github.com>
2023-08-25 14:53:58 -0300

    array

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function converter(){
-    var nome = (document.getElementById("nome").value)
-    var idade = (document.getElementById("idade").value)
-    var resultado = 0
-    var endereco = (document.getElementById("endereco").value)
+let nomes = []
+let professoes = []
+let nome = document.getElementById("nome");
+let prof = document.getElementById("prof");
 
-    resultado = "Seu nome é: " + nome 
-    resultado2 = "Sua idade é: " + idade
-    resultado3 = " Seu endereço é: " + endereco  
-    document.getElementById("resultado").innerHTML = resultado
-    document.getElementById("resultado2").innerHTML = resultado2
-    document.getElementById("resultado3").innerHTML = resultado3
+
+function adc() {
+    nomes.push(nome.value);
+    professoes.push(prof.value);
+    console.log(nomes, professoes);
 }
+function rmv() {
+    let pos = nomes.indexOf(nome.value);
+    if (pos !== -1) {
+        nomes.splice(pos, 1);
+        professoes.splice(pos, 1);
+
+
+    }
+    else {
+        console.log("O nome " + nome + "não foi encontrado")
+    }
+    console.log(nome, professoes);
+
+}
+
